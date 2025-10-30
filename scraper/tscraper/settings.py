ce54cdfbeb9b7a3873b4422ae6f3828908280070
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-10-30 21:11:47 +1300

    Update settings.py

--- scraper/tscraper/settings.py
+++ scraper/tscraper/settings.py
@@ -8,7 +8,7 @@ AUTOTHROTTLE_ENABLED = True
 CONCURRENT_REQUESTS = 8
 
 DEFAULT_REQUEST_HEADERS = {
-    "User-Agent": "TravelScoutBot/1.0 (+contact: brett.strawbridge@tandemtravel.co.nz)"
+    "User-Agent": "TravelScoutBot/1.0 (+contact: brettstrawbridge@windowslive.com)"
 }
 
 HTTPCACHE_ENABLED = True
