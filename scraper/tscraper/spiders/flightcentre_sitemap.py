f122ebed21267c21902e15deee9f0ae4e71b7787
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-11-04 13:54:38 +1300

    Update flightcentre_sitemap.py

--- scraper/tscraper/spiders/flightcentre_sitemap.py
+++ scraper/tscraper/spiders/flightcentre_sitemap.py
@@ -1,16 +1,29 @@
-import re, scrapy
+import re
+import scrapy
 from scrapy.spiders import SitemapSpider
-from tscraper.spiders.flightcentre import FlightCentreSpider, PRODUCT_RE, HOLIDAY_RE
+from tscraper.spiders.flightcentre import FlightCentreSpider
 
 class FlightCentreSitemapSpider(SitemapSpider, FlightCentreSpider):
     name = "flightcentre_sitemap"
     allowed_domains = ["flightcentre.co.nz", "www.flightcentre.co.nz"]
-    sitemap_urls = ["https://www.flightcentre.co.nz/sitemap.xml"]
-    # only follow detail pages
+
+    # Discover sitemaps from robots + common sitemap endpoints
+    sitemap_urls = [
+        "https://www.flightcentre.co.nz/robots.txt",
+        "https://www.flightcentre.co.nz/sitemap.xml",
+        "https://www.flightcentre.co.nz/sitemap_index.xml",
+    ]
+
+    # Only follow sitemaps that look relevant (reduces noise)
+    sitemap_follow = [r"(?i)holidays", r"(?i)product"]
+    sitemap_alternate_links = True
+
+    # Only detail pages
     sitemap_rules = [
         (r"/product/\d+/?$", "parse_detail"),
         (r"/holidays/.+-NZ\d+/?$", "parse_detail"),
     ]
+
     custom_settings = {
         "ROBOTSTXT_OBEY": True,
         "USER_AGENT": "TravelScoutBot/1.0 (+contact: data@travelscout.example)",
