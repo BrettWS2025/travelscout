c8e16e732ffe897868dda933992769b5e6a52369
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-11-11 10:23:14 +1300

    Update page.tsx

--- app/(marketing)/compare/best-time-to-book/page.tsx
+++ app/(marketing)/compare/best-time-to-book/page.tsx
@@ -1,1 +1,16 @@
+import WhenToBookCarousel from "@/components/WhenToBookCarousel";
 
+import dynamic from "next/dynamic";
+const DomesticFlightBookingTimingChart = dynamic(
+  () => import("@/components/DomesticFlightBookingTimingChart"),
+  { ssr: false }
+);
+const AusPacificFlightBookingTimingChart = dynamic(
+  () => import("@/components/AusPacificFlightBookingTimingChart"),
+  { ssr: false }
+);
+// NEW: International
+const InternationalFlightBookingTimingChart = dynamic(
+  () => import("@/components/InternationalFlightBookingTimingChart"),
+  { ssr: false }
+);
