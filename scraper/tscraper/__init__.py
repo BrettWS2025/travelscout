d44aa51cc7a8799f921ff6b428a33b9c3cc4545a
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-11-29 11:53:02 +1300

    Update __init__.py

--- scraper/tscraper/__init__.py
+++ scraper/tscraper/__init__.py
@@ -1,1 +1,7 @@
+# scraper/tscraper/__init__.py
+"""
+Package initializer for the TravelScout Scrapy project.
+Having this file ensures 'tscraper' is importable as a package.
+"""
+__all__ = ["spiders"]
 
