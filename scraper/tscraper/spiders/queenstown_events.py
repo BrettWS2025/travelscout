0bfcf37cf621846f01ede2d993203d20351c2ca7
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-11-30 19:28:23 +1300

    Update queenstown_events.py

--- scraper/tscraper/spiders/queenstown_events.py
+++ scraper/tscraper/spiders/queenstown_events.py
@@ -43,10 +43,10 @@ class GenericEventsSpider(scrapy.Spider):
     # ---------- init & config ----------
     def __init__(
         self,
-        base: str = "https://www.queenstownnz.co.nz/things-to-do/events/",
+        base: str = "https://www.queenstownnz.co.nz/things-to-do/events/event-calendar/",
         domain: str = "queenstownnz.co.nz",
-        allow: str = '^https://(?:www\\.)?queenstownnz\\.co.nz/things-to-do/events/[^/?#]+$',
-        linksel: str = "a[href*='/things-to-do/events/']::attr(href)",
+        allow: str = '^https://(?:www\\.)?queenstownnz\\.co.nz/things-to-do/events/event-calendar/[^/?#]+$',
+        linksel: str = "a[href*='/things-to-do/events/event-calendar/']::attr(href)",
         more: str = "",
         load_more: str = "60",
         pages: str | None = None,
