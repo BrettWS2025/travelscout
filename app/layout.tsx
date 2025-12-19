e523f3e38bd529354f4c62dedfb33d9c53475b70
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-12-19 18:46:04 +1300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
+import "react-day-picker/dist/style.css"; // DayPicker first
 import "../styles/globals.css";
-import "react-day-picker/dist/style.css";
 import type { ReactNode } from "react";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
