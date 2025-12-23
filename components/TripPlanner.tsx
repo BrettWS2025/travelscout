efdb8f0bb866b50b726a53b36e1aaf82e1dfa4f0
BrettWS2025 <brett.strawbridge1@gmail.com>
2025-12-23 15:39:01 +1300

    Update TripPlanner.tsx

--- components/TripPlanner.tsx
+++ components/TripPlanner.tsx
@@ -1,49 +1,68 @@
 "use client";
 
-import PlannerForm from "@/components/trip-planner/PlannerForm";
+import WhereWhenPicker from "@/components/trip-planner/WhereWhenPicker";
+import WaypointsSection from "@/components/trip-planner/WaypointsSection";
+import DraftItinerary from "@/components/trip-planner/DraftItinerary";
 import RouteOverview from "@/components/trip-planner/RouteOverview";
 import TripSummary from "@/components/trip-planner/TripSummary";
-import DraftItinerary from "@/components/trip-planner/DraftItinerary";
-import useTripPlanner from "@/lib/trip-planner/useTripPlanner";
+import { useTripPlanner } from "@/lib/trip-planner/useTripPlanner";
 
 export default function TripPlanner() {
   const tp = useTripPlanner();
 
   return (
-    <div className="space-y-6">
-      <PlannerForm
-        startCityId={tp.startCityId}
-        endCityId={tp.endCityId}
-        returnToStart={tp.returnToStart}
-        startQuery={tp.startQuery}
-        endQuery={tp.endQuery}
-        startDate={tp.startDate}
-        endDate={tp.endDate}
-        selectedRange={tp.selectedRange}
-        isLoading={tp.isLoading}
-        errorMsg={tp.errorMsg}
-        startResults={tp.startResults}
-        endResults={tp.endResults}
-        activePill={tp.activePill}
-        showWherePopover={tp.showWherePopover}
-        showCalendar={tp.showCalendar}
-        mobileSheetOpen={tp.mobileSheetOpen}
-        whereRef={tp.whereRef}
-        whenRef={tp.whenRef}
-        setStartQuery={tp.setStartQuery}
-        setEndQuery={tp.setEndQuery}
-        setStartDate={tp.setStartDate}
-        setEndDate={tp.setEndDate}
-        onOpenWhere={tp.openWhereDesktop}
-        onOpenWhen={tp.openWhenDesktop}
-        onOpenMobileSheet={tp.openMobileSheet}
-        onCloseMobileSheet={tp.closeMobileSheet}
-        onSelectStart={tp.selectStartCity}
-        onSelectEnd={tp.selectEndCity}
-        onToggleReturnToStart={tp.selectReturnToStart}
-        onRangeChange={tp.handleDateRangeChange}
-        onSubmit={tp.handleSubmit}
-      />
+    <div className="space-y-8">
+      <form onSubmit={tp.handleSubmit} className="card p-4 md:p-6 space-y-6">
+        <WhereWhenPicker
+          startCityId={tp.startCityId}
+          endCityId={tp.endCityId}
+          returnToStart={tp.returnToStart}
+          startQuery={tp.startQuery}
+          endQuery={tp.endQuery}
+          startDate={tp.startDate}
+          endDate={tp.endDate}
+          selectedRange={tp.selectedRange}
+          isLoading={tp.isLoading}
+          errorMsg={tp.errorMsg}
+          startResults={tp.startResults}
+          endResults={tp.endResults}
+          activePill={tp.activePill}
+          showWherePopover={tp.showWherePopover}
+          showCalendar={tp.showCalendar}
+          mobileSheetOpen={tp.mobileSheetOpen}
+          whereRef={tp.whereRef}
+          whenRef={tp.whenRef}
+          setStartQuery={tp.setStartQuery}
+          setEndQuery={tp.setEndQuery}
+          setStartDate={tp.setStartDate}
+          setEndDate={tp.setEndDate}
+          onOpenWhere={tp.openWhereDesktop}
+          onOpenWhen={tp.openWhenDesktop}
+          onOpenMobileSheet={tp.openMobileSheet}
+          onCloseMobileSheet={tp.closeMobileSheet}
+          onSelectStart={tp.selectStartCity}
+          onSelectEnd={tp.selectEndCity}
+          onToggleReturnToStart={tp.selectReturnToStart}
+          onRangeChange={tp.handleDateRangeChange}
+        />
+
+        <WaypointsSection
+          waypoints={tp.waypoints}
+          setWaypoints={tp.setWaypoints}
+          suggestedWaypoints={tp.suggestedWaypoints}
+          isLoading={tp.isLoading}
+        />
+
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="btn btn-accent px-6 py-3"
+            disabled={tp.isLoading}
+          >
+            {tp.isLoading ? "Generating..." : "Generate itinerary"}
+          </button>
+        </div>
+      </form>
 
       {tp.hasSubmitted && tp.plan.days.length > 0 && (
         <DraftItinerary
